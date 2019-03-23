1e374718ff31869065f8172bfb98dc99a2227fe2
katsukiwataru <a17dc050@dhw.ac.jp>
2019-03-24 01:11:43 +0900

    update index.vue

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   head: {
     title: 'sample',
+    titleTemplate: '%s | Nuxt.js tag items viewer',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
---
+++ pages/users/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>/user/index.vue</p>
+  </div>
+</template>
+
+<script>
+import {mapGetters} from 'vuex'
+
+export default {
+  async asyncData({store}) {
+    if(store.getters['items'].length) {
+      return
+    }
+  await store.dispatch('fetchItems')
+  },
+  computed: {
+    ...mapGetters(['items'])
+  },
+}
+</script>
